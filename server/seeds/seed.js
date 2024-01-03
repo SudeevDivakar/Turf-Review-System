41cc4de60048d80a6d1a84bd4f32fa775814bd3c
Sudeev Divakar <sudeev.divakar@gmail.com>
2024-01-03 15:02:06 +0530

    Fix deleteMany({}) problem

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Turf = require('../models/turfSchema.js');             //requiring model 
+const Review = require('../models/reviewSchema.js');
+const Turf = require('../models/turfSchema.js');             //requiring models
 const {footballTurfs} = require('./turfs.js');              //requiring file with seed data
 require('dotenv').config({ path: '../.env' });
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.DB_URL)    //connection to mongoDB
 
 async function seedDB() {                         //adding each football turf into the DB 
     await Turf.deleteMany({});
+    await Review.deleteMany({});
     for(let i of footballTurfs){
         const turf = new Turf(i);
         await turf.save();
