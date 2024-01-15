383cf3f09bac8d2ea337fee820972bee548d9681
Sudeev Divakar <sudeev.divakar@gmail.com>
2024-01-15 18:14:00 +0530

    Add Footer Credentials

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,18 +3,45 @@ import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Footer() {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#303030', marginTop: 'auto' }}>
       <Container>
-        <Toolbar>
-          <Typography variant="body1" color="inherit">
-            &copy;Turf Review
-          </Typography>
+        <Toolbar sx={{ dispay: 'flex', justifyContent: 'space-between' }}>
+          <div>
+          <Typography variant="body1" color="inherit" sx={{display: 'inline'}}>
+              GitHub Code Link:
+            </Typography>
+            <IconButton color="inherit" href="" target="_blank">
+              <GitHubIcon />
+            </IconButton>
+          </div>
+          <div>
+            <Typography variant="body1" color="inherit">
+              &copy;Turf Review
+            </Typography>
+          </div>
+          <div>
+            <Typography variant="body1" color="inherit" sx={{display: 'inline'}}>
+              Creator Handles : 
+            </Typography>
+            <IconButton color="inherit" href="https://github.com/SudeevDivakar" target="_blank">
+              <GitHubIcon />
+            </IconButton>
+            <IconButton color="inherit" href="https://www.linkedin.com/in/sudeev-divakar-66116026a/" target="_blank">
+              <LinkedInIcon />
+            </IconButton>
+            <IconButton color="inherit" href="https://www.instagram.com/sudeev_gb?igsh=NGl0YXJqMXpodjk5" target="_blank">
+              <InstagramIcon />
+            </IconButton>
+          </div>
         </Toolbar>
       </Container>
     </AppBar>
   );
 };
-
